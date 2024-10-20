51b6a859bfc9d1a7c2f30509ced84e3f447cebe3
pieromarcoz <75951868+pieromarcoz@users.noreply.github.com>
2024-10-19 23:50:40 -0500

    analytics vercel

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import ResponseDisplay from "./components/ResponseDisplay.tsx";
 import {useAuth} from "./hooks/useAuth.ts";
 import Loading from "./components/Loading.tsx";
 import FirstLoading from "./components/FirstLoading.tsx";
-import { Analytics} from "@vercel/analytics/react";
 
 function App() {
     const [changeForm, setChangeForm] = useState<boolean>(false)
@@ -43,34 +42,31 @@ function App() {
     }
 
     return (
-        <>
-            <main className={'flex flex-col justify-center min-h-screen bg-gray-100'}>
-                <header className="text-center mb-10">
-                    <h1 className={'text-3xl font-bold mx-auto'}>Authentication Service</h1>
-                    <p className="text-sm text-gray-500 mt-1">Powered by Laravel & React</p>
-                </header>
-                <div
-                    className={'max-w-2xl w-full mx-auto bg-white py-12 px-5 rounded-lg shadow-md flex flex-col items-center gap-7'}>
-                    {changeForm ? <RegisterForm/> : <LoginForm/>}
-                    <div className={'flex gap-3'}>
-                        <button
-                            className={'minimalist bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'}
-                            onClick={() => handleFormReset(false)}
-                        >
-                            Login
-                        </button>
-                        <button
-                            className={'minimalist bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'}
-                            onClick={() => handleFormReset(true)}
-                        >
-                            Register
-                        </button>
-                    </div>
-                    {isLoading ? <Loading/> : <ResponseDisplay response={response}/>}
+        <main className={'flex flex-col justify-center min-h-screen bg-gray-100'}>
+            <header className="text-center mb-10">
+                <h1 className={'text-3xl font-bold mx-auto'}>Authentication Service</h1>
+                <p className="text-sm text-gray-500 mt-1">Powered by Laravel & React</p>
+            </header>
+            <div
+                className={'max-w-2xl w-full mx-auto bg-white py-12 px-5 rounded-lg shadow-md flex flex-col items-center gap-7'}>
+                {changeForm ? <RegisterForm/> : <LoginForm/>}
+                <div className={'flex gap-3'}>
+                    <button
+                        className={'minimalist bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'}
+                        onClick={() => handleFormReset(false)}
+                    >
+                        Login
+                    </button>
+                    <button
+                        className={'minimalist bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'}
+                        onClick={() => handleFormReset(true)}
+                    >
+                        Register
+                    </button>
                 </div>
-            </main>
-            <Analytics/>
-        </>
+                {isLoading ? <Loading/> : <ResponseDisplay response={response}/>}
+            </div>
+        </main>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {AuthProvider} from "./context/AuthContext.tsx";
+import { Analytics} from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')!).render(
 <AuthProvider>
     <App />
+    <Analytics/>
 </AuthProvider>
 )
